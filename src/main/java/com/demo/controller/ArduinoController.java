4f567003f0d4704cdceb522793853b25d0e9d6ba
Trần Mạnh Công <congtm@nganluong.vn>
2019-12-11 20:02:45 +0700

    update

--- src/main/java/com/demo/controller/ArduinoController.java
+++ src/main/java/com/demo/controller/ArduinoController.java
@@ -29,23 +29,28 @@ public class ArduinoController {
 	@Autowired
 	private ArduinoService arduinoService;
 	
+	// Get Status
 	@RequestMapping(value="/status",method = RequestMethod.GET)
 	@ResponseBody
 	public int getStatus() {
 		return STATUS;
 	}
-	
+
+	// Get Arduino Info
 	@RequestMapping(value="/arduino-info",method = RequestMethod.GET)
 	@ResponseBody
 	public ArduinoInfo getArduinoInfo(@RequestParam(name="id")int id) {
 		return arduinoService.getAdruinoInfo(id);
 	}
+
+	// Get Sensor Info
 	@RequestMapping(value="/sensor-info",method = RequestMethod.GET)
 	@ResponseBody
 	public SensorInfo getSensorInfo(@RequestParam(name="id")int id) {
 		return arduinoService.getSensorInfo(id);
 	}
 	
+	// Change status
 	@RequestMapping(value="/change-status",method = RequestMethod.POST)
 	@ResponseBody
 	public int changeStatus(@RequestParam("status")int status) {
@@ -53,11 +58,11 @@ public class ArduinoController {
 		return STATUS;
 	}
 	
-	@RequestMapping(value="/all-log",method = RequestMethod.GET)
-	@ResponseBody
-	public List<Arduino> getAllArduinoLogs() {
-		return arduinoService.getAll();
-	}
+	// @RequestMapping(value="/all-log",method = RequestMethod.GET)
+	// @ResponseBody
+	// public List<Arduino> getAllArduinoLogs() {
+	// 	return arduinoService.getAll();
+	// }
 	
 	@RequestMapping(value="/log",method = RequestMethod.GET)
 	@ResponseBody
@@ -65,24 +70,27 @@ public class ArduinoController {
 		return arduinoService.getByTime(time, time + 24*3600*1000l);
 	}
 	
-	@RequestMapping(value="/all",method = RequestMethod.GET)
-	@ResponseBody
-	public List<Arduino> searchAll(@RequestParam(name = "page") int page,@RequestParam(name = "pageSize") int pageSize) {
-		return arduinoService.searchAll(page, pageSize).getContent();
-	}
+	// @RequestMapping(value="/all",method = RequestMethod.GET)
+	// @ResponseBody
+	// public List<Arduino> searchAll(@RequestParam(name = "page") int page,@RequestParam(name = "pageSize") int pageSize) {
+	// 	return arduinoService.searchAll(page, pageSize).getContent();
+	// }
 	
+	// Search Log
 	@RequestMapping(value="/search",method = RequestMethod.POST)
 	@ResponseBody
 	public List<Arduino> search(@RequestBody ArduinoSearch search) {
 		return arduinoService.search(search);
 	}
 	
+	// Get Live
 	@RequestMapping(value="/live",method = RequestMethod.GET)
 	@ResponseBody
 	public List<Arduino> getLive(@RequestParam(name = "amount") int amount,@RequestParam(name = "arduinoId") int arduinoId,@RequestParam(name = "sensorId") int sensorId) {
 		return arduinoService.getLive(amount,arduinoId,sensorId);
 	}
 	
+	// Insert Log
 	@RequestMapping(value="/insert",method = RequestMethod.POST)
 	@ResponseBody
 	public int createArduinoLog(@RequestBody ArduinoInput input) {
